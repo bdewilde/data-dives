dc3523153f7b74e8b896ede44076b5d7e2c1c707
Burton DeWilde <burtdewilde@gmail.com>
2021-01-30 17:07:55 -0600

    Add util func for getting filename from url

--- src/time_series_forecasting_study/utils.py
+++ src/time_series_forecasting_study/utils.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import pathlib
+import urllib.parse
+from typing import Optional
 
 
 def to_path(str_or_path: str | pathlib.Path) -> pathlib.Path:
@@ -11,3 +13,14 @@ def to_path(str_or_path: str | pathlib.Path) -> pathlib.Path:
         return str_or_path
     else:
         raise TypeError()
+
+
+def get_fname_from_url(url: str) -> Optional[str]:
+    """
+    Extract a filename from a URL's path, if possible.
+
+    Warning:
+        This function doesn't have guardrails, so only use if you know you have a valid
+        filename at the end of the URL path.
+    """
+    return pathlib.Path(urllib.parse.urlparse(url).path).name
