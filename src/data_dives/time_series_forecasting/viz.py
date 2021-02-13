264fa6353f69eb402cbefb5fde4c3e45e21b025c
Burton DeWilde <burtdewilde@gmail.com>
2021-02-13 12:45:38 -0600

    Add seasonal plot to tsf viz, standardize kw args

--- src/data_dives/time_series_forecasting/viz.py
+++ src/data_dives/time_series_forecasting/viz.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import itertools
 from typing import Any, Dict, List, Optional, Sequence, Sequence
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -14,8 +15,8 @@ def plot_time_series(
     mseries: Sequence[pd.Series],
     labels: Optional[Sequence[str]] = None,
     subplots: bool = True,
-    plot_kwargs: Optional[Dict[str, Any]] = None,
-    **fig_kwargs,
+    plot_kw: Optional[Dict[str, Any]] = None,
+    **fig_kw,
 ) -> plt.Subplot | List[plt.Subplot]:
     """
     Make time plots for multiple time series, either on separate subplots or overlaid.
@@ -26,27 +27,70 @@ def plot_time_series(
             if ``subplots`` is True or legend labels otherwise.
         subplots: If True, plot each series separately on subplots; otherwise, plot
             them all together on a single axis.
-        plot_kwargs: Keyword arguments passed on to :func:`matplotlib.axes.Axes.plot()`.
-        **fig_kwargs: Keyword arguments passed on to :func:`plt.figure()`.
+        plot_kw: Keyword arguments passed on to :func:`matplotlib.axes.Axes.plot()`.
+        **fig_kw: Keyword arguments passed on to :func:`plt.figure()`.
 
     Returns:
         Subplot (or a list thereof) on which series have been plotted.
     """
     labels = itertools.repeat(None, len(mseries)) if labels is None else labels
-    plot_kwargs = {} if plot_kwargs is None else plot_kwargs
+    plot_kw = {} if plot_kw is None else plot_kw
     if subplots is True:
-        _, axes = plt.subplots(nrows=len(mseries), sharex=True, **fig_kwargs)
+        _, axes = plt.subplots(nrows=len(mseries), sharex=True, **fig_kw)
         for ax, series, label in zip(axes, mseries, labels):
-            _ = series.plot.line(ax=ax, ylabel=label, **plot_kwargs)
+            _ = series.plot.line(ax=ax, ylabel=label, **plot_kw)
             # ax.xaxis_date()
         return list(axes)
     else:
-        _, ax = plt.subplots(**fig_kwargs)
+        _, ax = plt.subplots(**fig_kw)
         for series, label in zip(mseries, labels):
-            _ = series.plot.line(ax=ax, label=label, **plot_kwargs)
+            _ = series.plot.line(ax=ax, label=label, **plot_kw)
         # ax.xaxis_date()
         _ = ax.legend(loc="best")
         return ax
+
+
+def plot_seasonal_periods(
+    data_grped: pd.core.groupby.DataFrameGroupBy,
+    dt_attr: str,  # Literal["hour", day", "month", "quarter", "year"],
+    *,
+    cmap: Optional[str] = None,
+    plot_kw: Optional[Dict[str, Any]] = None,
+    **fig_kw,
+) -> plt.Subplot:
+    """
+    Make a plot of each seasonal group in ``data_grped`` laid one on top of the other,
+    with optional colormap (+colorbar).
+
+    Args:
+        data_grped
+        dt_attr: Datetime component accessible as group index attribute.
+        cmap: Name of cmap to use in coloring each seasonal periods' line.
+        plot_kw: Keyword arguments passed on to :func:`matplotlib.axes.Axes.plot()`.
+        **fig_kw: Keyword arguments passed on to :func:`plt.figure()`.
+
+    Returns:
+        Subplot on which seasonal periods have been plotted.
+    """
+    fig, ax = plt.subplots(**fig_kw)
+    plot_kw = {} if plot_kw is None else plot_kw
+    if cmap is not None:
+        cmap = plt.cm.get_cmap(name=cmap)
+        group_names = data_grped.groups.keys()
+        cmap_norm = mpl.colors.Normalize(vmin=min(group_names), vmax=max(group_names))
+        _ = fig.colorbar(plt.cm.ScalarMappable(norm=cmap_norm, cmap=cmap), ax=ax)
+    ngroups = data_grped.ngroups
+    minx = None
+    maxx = None
+    for i, (name, grp) in enumerate(data_grped):
+        xvals = getattr(grp.index, dt_attr)
+        yvals = grp.values
+        color = cmap(i / ngroups) if cmap else None
+        _ = ax.plot(xvals, yvals, color=color, label=name, **plot_kw))
+        minx = min(minx, min(xvals)) if minx is not None else min(xvals)
+        maxx = max(maxx, max(xvals)) if maxx is not None else max(xvals)
+    _ = ax.set_xlim(left=minx, right=maxx)
+    return ax
 
 
 def plot_autocorrelations(
@@ -56,9 +100,9 @@ def plot_autocorrelations(
     alpha: float = 0.05,
     use_vlines: bool = True,
     zero: bool = False,
-    acf_kwargs: Optional[Dict[str, Any]] = None,
-    pacf_kwargs: Optional[Dict[str, Any]] = None,
-    **fig_kwargs,
+    acf_kw: Optional[Dict[str, Any]] = None,
+    pacf_kw: Optional[Dict[str, Any]] = None,
+    **fig_kw,
 ) -> List[plt.Subplot]:
     """
     Plot full and partial autocorrelations for a time series in a single figure
@@ -69,9 +113,9 @@ def plot_autocorrelations(
         lags
         alpha
         use_vlines
-        acf_kwargs
-        pacf_kwargs
-        **fig_kwargs: Keyword arguments passed on to :func:`plt.figure()`.
+        acf_kw
+        pacf_kw
+        **fig_kw: Keyword arguments passed on to :func:`plt.figure()`.
 
     Returns:
         Subplots on which autocorrelations have been plotted, as a list.
@@ -80,22 +124,22 @@ def plot_autocorrelations(
         - :func:`sm.graphics.tsa.plot_acf()`
         - :func:`sm.graphics.tsa.plot_pacf()`
     """
-    acf_kwargs = {} if acf_kwargs is None else acf_kwargs
-    pacf_kwargs = {} if pacf_kwargs is None else pacf_kwargs
-    if "title" not in acf_kwargs:
-        acf_kwargs["title"] = None
-    if "title" not in pacf_kwargs:
-        pacf_kwargs["title"] = None
-    _, axes = plt.subplots(nrows=2, sharex=True, **fig_kwargs)
+    acf_kw = {} if acf_kw is None else acf_kw
+    pacf_kw = {} if pacf_kw is None else pacf_kw
+    if "title" not in acf_kw:
+        acf_kw["title"] = None
+    if "title" not in pacf_kw:
+        pacf_kw["title"] = None
+    _, axes = plt.subplots(nrows=2, sharex=True, **fig_kw)
     _ = sm.graphics.tsa.plot_acf(
         series,
         lags=lags, alpha=alpha, use_vlines=use_vlines, zero=zero, ax=axes[0],
-        **acf_kwargs
+        **acf_kw
     )
     _ = sm.graphics.tsa.plot_pacf(
         series,
         lags=lags, alpha=alpha, use_vlines=use_vlines, zero=zero, ax=axes[1],
-        **pacf_kwargs
+        **pacf_kw
     )
     axes[0].set_ylabel("acf")
     axes[1].set_ylabel("pacf")
@@ -107,7 +151,7 @@ def plot_residuals_diagnostics(
     residuals: pd.Series,
     acf_kw=None,
     hist_kw=None,
-    **fig_kwargs
+    **fig_kw
 ) -> List[plt.Subplot]:
     """
     Make three diagnostic plots of residuals to evaluate a model's fit: residuals as
@@ -118,12 +162,12 @@ def plot_residuals_diagnostics(
         residuals
         acf_kw
         hist_kw
-        **fig_kwargs
+        **fig_kw
 
     Returns:
         Subplots on which residuals diagnostics have been plotted.
     """
-    fig = plt.figure(**fig_kwargs)
+    fig = plt.figure(**fig_kw)
     gs = fig.add_gridspec(nrows=2, ncols=2)
     ax1 = fig.add_subplot(gs[0, :])
     ax2 = fig.add_subplot(gs[1, 0])
